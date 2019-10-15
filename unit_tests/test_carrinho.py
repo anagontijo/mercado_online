c7c703d80b41de0e482dd9a59e47ae026bdeb01d
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-15 01:10:10 -0300

    Correção dos testes da classe carrinho

--- unit_tests/test_carrinho.py
+++ unit_tests/test_carrinho.py
@@ -1,16 +1,21 @@
 import unittest
 import sys
 import os
+from collections import defaultdict
 sys.path.insert(1, os.path.abspath('')[:-10])
-import classes
+import classes, BD
 
 class TestCarrinho(unittest.TestCase):
     def setUp(self):
         self.shopcarts = []
         # Preço do produto padrão a ser testado
         self.aux_price = 7.09
-        self.cat = classes.Catalogo()
-        itens = [{1:1,2:2,3:3},{10:1,20:2,30:3},{10:1,30:2,50:3}]
+        self.cat = BD.Catalogo()
+        itens = [defaultdict(int), defaultdict(int), defaultdict(int)]
+        itens_aux = [{(1,1),(2,2),(3,3)},{(10,1),(20,2),(30,3)},[(10,1),(30,2),(50,3)]];
+        for i, list in enumerate(itens_aux):
+            for item in list:
+                itens[i][item[0]] = item[1]
         precos = [20 + (2 * 3.5) + (3 * 3.8),
                   13.9 + (2 * 2.53) + (3 * 7.09),
                   13.9 + (2 * 7.09) + (3 * 4.19)]
@@ -27,20 +32,22 @@ class TestCarrinho(unittest.TestCase):
             cart.adicionar_produto(cod_produto=30,qtd=1)
             self.assertEqual(cart.itens[30],results[i])
             self.assertEqual(cart.preco_total, price_before + self.aux_price)
-        self.assertRaises("Estoque insuficiente.",
-                          self.shopcarts[0].adicionar_estoque(cod_produto=52,
-                                                              qtd=101))
+        self.assertRaises(classes.stockException,
+                          self.shopcarts[0].adicionar_produto,cod_produto=52,
+                                                              qtd=101)
 
     def test_remover_produto(self):
-        results = [0,3,2]
-        self.assertRaises("Este produto não está no carrinho.", self.shopcarts[0].remover_produto(cod_produto=30, qtd=1))
+        results = [3,2]
+        self.assertRaises(classes.cartException, self.shopcarts[0].remover_produto,cod_produto=30, qtd=1)
         for i, cart in enumerate(self.shopcarts[1:]):
             for j in range(5):
                 price_before = cart.preco_total
                 cart.remover_produto(cod_produto=30, qtd=j)
                 self.assertEqual(cart.itens[30], max(results[i]-j,0))
                 self.assertEqual(cart.preco_total, price_before -
-                                 (max(results[i]-j,0)*self.aux_price))
+                                 (min(j,results[i])*self.aux_price))
+                cart.itens[30] += min(j,results[i])
+                cart.preco_total += min(j,results[i])*self.aux_price
 
     def test_esvaziar(self):
         for cart in self.shopcarts:
@@ -50,8 +57,8 @@ class TestCarrinho(unittest.TestCase):
 
     def test_finaliza_compra(self):
         for cart in self.shopcarts:
-            cods = cart.itens.keys()
-            qtds = cart.itens.values()
+            cods = list(cart.itens.keys())
+            qtds = list(cart.itens.values())
             qtd_1 = self.cat.verifica_estoque(cods[0])
             qtd_2 = self.cat.verifica_estoque(cods[1])
             qtd_3 = self.cat.verifica_estoque(cods[2])
@@ -61,9 +68,9 @@ class TestCarrinho(unittest.TestCase):
 
             self.assertEqual(qtd_1, self.cat.verifica_estoque(cods[0]) +
                              qtds[0])
-            self.assertEqual(qtd_1, self.cat.verifica_estoque(cods[1]) +
+            self.assertEqual(qtd_2, self.cat.verifica_estoque(cods[1]) +
                              qtds[1])
-            self.assertEqual(qtd_1, self.cat.verifica_estoque(cods[2]) +
+            self.assertEqual(qtd_3, self.cat.verifica_estoque(cods[2]) +
                              qtds[2])
             self.assertEqual(price, result)
 
